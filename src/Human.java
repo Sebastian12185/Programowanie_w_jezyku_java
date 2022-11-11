8e95e7de33315dca742049c946cbd1a9508d3632
sebas <sebastian.jakubek01@gmail.com>
2022-11-11 17:57:01 +0100

    First_Lesson_Exercise_2

--- src/Human.java
+++ src/Human.java
@@ -1,5 +1,7 @@
 public class Human {
     String firstName, lastName;
+    Animal pet;
+    Car car;
     public String toString(){
         return "First Name: "+ this.firstName
                 +" Last Name: "+ this.lastName;
--- src/Main.java
+++ src/Main.java
@@ -9,8 +9,11 @@ public class Main {
         System.out.println("Waga psa: "+dog.weight);
         dog.feed();
         System.out.println("Waga psa: "+dog.weight);
+        me.pet = dog;
+        System.out.println(me.pet.specie);
         System.out.println(me);
         Car car = new Car("VW","GOLF",10.5,"Black",2007,"Sedan");
+        me.car = car;
         System.out.println(car);
     }
 }
